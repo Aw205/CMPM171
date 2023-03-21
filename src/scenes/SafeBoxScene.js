c64e506d18affe4d9b27e2b2a8bc9cbd70de9273
Chris Pau <chrispau.hk@gmail.com>
2023-03-20 18:10:05 -0700

    Safe Hint
    forgot to save this for last push

--- src/scenes/SafeBoxScene.js
+++ src/scenes/SafeBoxScene.js
@@ -6,7 +6,7 @@ class SafeBoxScene extends Phaser.Scene {
 
     create() {
         this.tw = new Typewriter(this,180,430);
-        this.scene.run("DialogModal", {text: "It seems to be locked. I guess I'll give a crack at it.\n\n [ENTER]", scene: "SafeBoxScene" });
+        this.scene.run("DialogModal", {text: "It seems to be a 4 digit code. I guess I'll give a crack at it.\n\n [ENTER]", scene: "SafeBoxScene" });
 
         this.formHtml = this.createInput();
         this.codeInput = this.add.dom(180, 410).createFromHTML(this.formHtml).setVisible(false);
@@ -21,7 +21,7 @@ class SafeBoxScene extends Phaser.Scene {
                this.scene.get("CrimeScene").events.emit("safeUnlocked");
             }
             else{
-               this.tw.write("Hmm not quite. Unfortunate.");
+               this.tw.write("Hmm not quite.");
             }
             this.confirmButton.disableInteractive();
             this.input.keyboard.manager.enabled = true;
